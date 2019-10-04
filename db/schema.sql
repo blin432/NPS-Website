8cc3c881679a9394812d655a9b37ff552a9bd65a
Benjamin Lin <blin432@gmail.com>
2019-10-04 18:57:57 -0400

    users schema

--- db/schema.sql
+++ db/schema.sql
@@ -5,10 +5,10 @@ USE nps_db;
 CREATE TABLE selections
 (
 	id int NOT NULL,
-	checked BOOLEAN NOT NULL,
+	users varchar NOT NULL,
 	PRIMARY KEY (id)
 );
 
-INSERT INTO selections (id, checked) VALUES (8, false), (9, false), (10, false), (11, false), (12, false), (13, false), (14, false), (15, false), (16, false), (17, false), (18, false), (19, false), (20, false), (21, false), (22, false), (23, false), (24, false), (25, false), (26, false), (27, false), (28, false), (29, false);
+INSERT INTO selections (id, users) VALUES (8, false), (9, false), (10, false), (11, false), (12, false), (13, false), (14, false), (15, false), (16, false), (17, false), (18, false), (19, false), (20, false), (21, false), (22, false), (23, false), (24, false), (25, false), (26, false), (27, false), (28, false), (29, false);
 
 SELECT * FROM selections;
