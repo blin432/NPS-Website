777a19857fe6e65e0c32718d68914634a691a8b6
Benjamin Lin <blin432@gmail.com>
2019-12-26 23:02:48 -0500

    thumbs up, need to finish login

--- public/app.js
+++ public/app.js
@@ -154,7 +154,7 @@ $.ajax({
                     "<div class='modal-body card-text' id='modal-des'><strong>Phone Number:</strong><br>" + parkPhone + "</div>" +
                     "<div class='modal-body card-text' id='modal-des'><strong>Email Address:</strong><br>" + parkEmail + "</div>" +
                     "<div class='modal-footer'>" +
-                        "<button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>" +
+                        "<img class='thumbs-pic' height='20px' width='20px' src='./images/pngguru.com.png'><img class='thumbs-pic' height='20px' width='20px' src='./images/PinClipart.com_walking-dead-clip-art_727656.png'> <button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>" +
                     "</div>" +
                 "</div>" +
             "</div>" +
@@ -202,70 +202,4 @@ function parksVisited() {
 
     $("#check-num").html(parkNum);
 }
-
-// funtions for auth (will refactor)
-//done in sequelize
-//coodinate with jaws DB mySQL
-
-
-///selector for html
-var signUpForm = document.getElementById("sign-up-form");
-var logInForm = document.getElementById("log-in-form");
-var loginButton = document.getElementById("log-in-button");
-var signUpButton = document.getElementById("sign-up-button");
-var createAccountButton = document.getElementById("create-account-button");
-var authBox = document.getElementById("auth-box");
-var existingMember = document.getElementById('existing-member');
-var signUpBox = document.getElementById("sign-up");
-
-var userEmail = document.getElementById("sign-up-email");
-var userPassword = document.getElementById("sign-up-password");
-var loginEmail = document.getElementById("login-email");
-var loginPw = document.getElementById("login-password");
-
-
-function showLoginForm(){
-    signUpButton.style.setProperty("display","none");
-    signUpBox.style.setProperty("display","none");
-    loginButton.style.setProperty("display","none");
-    authBox.style.setProperty("display","block");
-    logInForm.style.setProperty("display","block");
-}
-
-function showSignUpForm(){
-    existingMember.style.setProperty("display","none");
-    signUpButton.style.setProperty("display","none");
-    createAccountButton.style.setProperty("display","block");
-    loginButton.style.setProperty("display","none");
-    authBox.style.setProperty("display","block");
-    signUpForm.style.setProperty("display","block");
-}
-
-function cancel(){
-    location.reload();
-}
-
-function createNewAccount(){
-    axios.post('/auth/register', { username: userName.value, email: userEmail.value, password: userPassword.value }).then(function(response) {
-        var user = response.data.username;
-        console.log(user);
-        alert(`Account created for ${user}`);
-        location.reload();
-        alert("please login after creating account");
-    }).catch(function(){
-        alert(`Error in loggin in`);
-    });
-}
-
-function loginToExistingAccount(){
-    axios.post('/auth/login',{email:loginEmail.value,password:loginPw.value}).then(function(response){
-        console.log(response.data);
-        alert('success');
-        window.location = response.data.URL;
-    }).catch(function(err){
-        console.log(err);
-    });
-}
-
-
 
